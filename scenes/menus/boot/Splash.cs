a5f4f743f3a2131b9fc82c97e82e0a4d4abf8328
Brendan <brendanweirtz@gmail.com>
2022-01-04 15:07:54 -0800

    Splash screen fix

--- scenes/menus/boot/Splash.cs
+++ scenes/menus/boot/Splash.cs
@@ -4,19 +4,22 @@ using System;
 public class Splash : CanvasLayer
 {
 
-    public override async void _Ready()
-    {
-        var animPlayer = (AnimationPlayer)GetNode("Cover/AnimationPlayer");
-        var timer = (Timer)GetNode("Timer");
-        var audioPlayer = (AudioStreamPlayer)GetNode("AudioStreamPlayer");
-        timer.Connect("timeout", this, "OnTimerTimeout");
-        animPlayer.Play("logo_reveal");
-    }
+	public override async void _Ready()
+	{
+		var animPlayer = (AnimationPlayer)GetNode("Cover/AnimationPlayer");
+		var timer = (Timer)GetNode("Timer");
+		var audioPlayer = (AudioStreamPlayer)GetNode("AudioStreamPlayer");
+		timer.Connect("timeout", this, "OnTimerTimeout");
+		animPlayer.Play("logo_reveal");
+	}
 
-    public void OnTimerTimeout() 
-    {
-        var mainMenu = (PackedScene)ResourceLoader.Load("res://scenes/menus/main/MainMenu.tscn");
-        GetTree().ChangeSceneTo(mainMenu);
-        this.QueueFree();
-    }
+	public void _on_Timer_timeout()
+	{
+		var mainMenu = (PackedScene)ResourceLoader.Load("res://scenes/menus/main/MainMenu.tscn");
+			GetTree().ChangeSceneTo(mainMenu);
+			this.QueueFree();
+	}
+
 }
+
+
